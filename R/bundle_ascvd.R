df3474f2171116a86fbf2dd709ca19d3a3a22f05
jarrod-dalton <jarrod-dalton@users.noreply.github.com>
2025-12-23 18:27:33 -0500

    add derived_vars_ascvd.R

--- R/bundle_ascvd.R
+++ R/bundle_ascvd.R
@@ -1,10 +1,28 @@
 # ------------------------------------------------------------------------------
-# ASCVD ModelBundle constructor
+# ascvd_model_bundle(params = list())
+#
+# Build the patientSimASCVD ModelBundle.
+#
+# init_patient() is called once at the start of Engine$run() and is used to
+# register derived variables (idempotent by name).
 # ------------------------------------------------------------------------------
 ascvd_model_bundle <- function(params = list()) {
+
+  init_patient <- function(patient, ctx) {
+    patientSimCore::check_derived(patient, derived_vars_ascvd(params), replace = FALSE)
+    invisible(NULL)
+  }
+
+  with_params <- function(ctx) {
+    if (is.null(ctx)) ctx <- list()
+    if (is.null(ctx$params)) ctx$params <- params
+    ctx
+  }
+
   list(
-    propose_events = propose_events_ascvd,
-    transition = transition_ascvd,
-    stop = stop_ascvd
+    init_patient  = init_patient,
+    propose_events = function(patient, ctx) propose_events_ascvd(patient, with_params(ctx)),
+    transition     = function(patient, event, ctx) transition_ascvd(patient, event, with_params(ctx)),
+    stop           = function(patient, event, ctx) stop_ascvd(patient, event, with_params(ctx))
   )
 }
