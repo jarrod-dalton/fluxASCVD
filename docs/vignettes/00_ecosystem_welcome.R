d788a9ebd247bb8d2ae7fe536abc1a181b06a4b7
jarrod-dalton <jarrod-dalton@users.noreply.github.com>
2026-04-22 07:04:02 -0400

    Remove package-level docs/vignettes after super-repo tutorial consolidation

--- docs/vignettes/00_ecosystem_welcome.R
+++
@@ -1,7 +0,0 @@
-# fluxASCVD vignette source moved to canonical super-repo tutorials path.
-#
-# Canonical tutorial:
-# https://github.com/jarrod-dalton/flux/tree/main/tutorials/06_ascvd_ecosystem_welcome.R
-#
-# If working from a full super-repo clone, open:
-# ../../tutorials/06_ascvd_ecosystem_welcome.R
--- docs/vignettes/01_prepare_ttv.R
+++
@@ -1,7 +0,0 @@
-# fluxASCVD vignette source moved to canonical super-repo tutorials path.
-#
-# Canonical tutorial:
-# https://github.com/jarrod-dalton/flux/tree/main/tutorials/07_ascvd_prepare_ttv.R
-#
-# If working from a full super-repo clone, open:
-# ../../tutorials/07_ascvd_prepare_ttv.R
